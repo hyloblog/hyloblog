hylodoc/hylodoc.com

chore: update homepage

internal/app/server.go
@@ -222,11 +222,17 @@ func index(r request.Request) (response.Response, error) {
 		[]string{"index.html"},
 		util.PageInfo{
 			Data: struct {
-				Title    string
-				UserInfo *session.UserInfo
+				Title       string
+				UserInfo    *session.UserInfo
+				HylodocLink string
 			}{
 				Title:    "Hylodoc - blogging for devs",
 				UserInfo: session.ConvertSessionToUserInfo(sesh),
+				HylodocLink: fmt.Sprintf(
+					"%s://%s",
+					config.Config.Hylodoc.Protocol,
+					config.Config.Hylodoc.RootDomain,
+				),
 			},
 		},
 	), nil

hyloblog/hyloblog

internal/app/server.go
@@ -222,11 +222,17 @@ func index(r request.Request) (response.Response, error) {
 		[]string{"index.html"},
 		util.PageInfo{
 			Data: struct {
-				Title    string
-				UserInfo *session.UserInfo
+				Title        string
+				UserInfo     *session.UserInfo
+				HyloblogLink string
 			}{
 				Title:    "Hyloblog - blogging for devs",
 				UserInfo: session.ConvertSessionToUserInfo(sesh),
+				HyloblogLink: fmt.Sprintf(
+					"%s://%s",
+					config.Config.Hyloblog.Protocol,
+					config.Config.Hyloblog.RootDomain,
+				),
 			},
 		},
 	), nil